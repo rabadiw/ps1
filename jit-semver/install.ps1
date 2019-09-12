fdc30226cb823c0dc6ab98f99442a54fc42d1cfe
Wael Rabadi <wrabadi@pivotal.io>
2019-09-12 14:40:58 -0500

    local install of jit-semver

---
+++ jit-semver/install.ps1
@@ -0,0 +1,5 @@
+param([switch]$WhatIf = $false, [switch]$Force = $false, [switch]$Verbose = $false)
+
+$installDir = Split-Path $MyInvocation.MyCommand.Path -Parent
+
+Import-Module $installDir\src\jit-semver.psd1
