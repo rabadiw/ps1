0c0193527de146d1f5df5221680571bc347d2207
Wael Rabadi <wrabadi@pivotal.io>
2019-10-01 01:19:08 -0500

    add prefix to get/set semver

--- jit-semver/src/jit-semver.psm1
+++ jit-semver/src/jit-semver.psm1
@@ -2,10 +2,10 @@
 
 function ConvertTo-SemVer {
     param(
-        [Parameter(Mandatory = $false, ValueFromPipeline = $true)][string]$SemVersion = (git describe --tags)
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true)][string]$Version = (git describe --tags)
     )
-    [regex]$rx = "(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)-?(?<pre>[a-zA-Z]+)?\.?(?<prepatch>\d+)?\+?(?<build>\d+)?"
-    $curver = $rx.Match($SemVersion)
+    [regex]$rx = "(?<prefix>.+)?(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)-?(?<pre>[a-zA-Z]+)?\.?(?<prepatch>\d+)?\+?(?<build>\d+)?"
+    $curver = $rx.Match($Version)
 
     @{
         Major    = [int]$curver.Groups["major"].Value
@@ -14,18 +14,23 @@ function ConvertTo-SemVer {
         Pre      = [string]$curver.Groups["pre"].Value
         PrePatch = [int]$curver.Groups["prepatch"].Value
         Build    = [int]$curver.Groups["build"].Value
+        Prefix   = [string]$curver.Groups["prefix"].Value
     }
 }
 
 function Format-SemVerString {
     param(
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)][hashtable]$SemVersion
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)][hashtable]$Version,
+        [Parameter(Mandatory = $false)][switch]$IncludePrefix
     )
 
-    $prevalue = if ($SemVersion.pre -ne "") { "-$($SemVersion.pre)" }
-    $prepatchvalue = if ($SemVersion.prepatch -ne "") { ".$($SemVersion.prepatch)" }
-    $buildvalue = if ($SemVersion.build -ne "") { "+$($SemVersion.build)" }
-    "$($SemVersion.major).$($SemVersion.minor).$($SemVersion.patch)${prevalue}${prepatchvalue}${buildvalue}"
+    $prevalue = if ($Version.pre -ne "") { "-$($Version.pre)" }
+    $prepatchvalue = if ($Version.prepatch -ne "") { ".$($Version.prepatch)" }
+    $buildvalue = if ($Version.build -ne "") { "+$($Version.build)" }
+    if (-Not($IncludePrefix)) {
+        $Version.Prefix = ""
+    }
+    "$($Version.Prefix)$($Version.major).$($Version.minor).$($Version.patch)${prevalue}${prepatchvalue}${buildvalue}"
 }
 
 function Get-SemVerOverride {
@@ -38,29 +43,31 @@ function Get-SemVer {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $false)][switch]$IncludePrefix,
-        [Parameter(Mandatory = $false)][ValidateNotNullOrEmpty()][string]$Prefix = "v"
+        [Parameter(Mandatory = $false)][ValidateNotNullOrEmpty()][string]$Prefix
     )
-    $SemVersion = $null
+    $Version = $null
     # TBD value
     #   if (Test-SemVerOverride) {
-    #     $SemVersion = (Get-SemVerOverride).Version | ConvertTo-SemVer
+    #     $Version = (Get-SemVerOverride).Version | ConvertTo-SemVer
     #   }
 
     if ((Test-SemVer -ShowMessage)) {
         # value of (git describe --tags)
-        $SemVersion = ConvertTo-SemVer
+        $Version = ConvertTo-SemVer
     }
     else {
         # default
         Write-Warning "Defaulting to 1.0.0-alpha."
-        $SemVersion = ConvertTo-SemVer -semver "1.0.0-alpha"
+        $Version = ConvertTo-SemVer -Version "1.0.0-alpha"
     }
 
-    $ver = $SemVersion | Format-SemVerString
     if ($IncludePrefix) {
-        $ver = "${Prefix}${ver}"
+        if (Test-String $Prefix) {
+            $Version.Prefix = $Prefix
+        }
     }
-    $ver
+
+    $Version | Format-SemVerString -IncludePrefix:$IncludePrefix
 }
 
 function Set-SemVer {
@@ -68,13 +75,14 @@ function Set-SemVer {
 
     param(
         [Parameter(Mandatory = $false)][ValidateSet("major", "minor", "patch", "build")][string]$SemVerb = $null,
-        [Parameter(Mandatory = $false)][string]$SemVersion = (Get-SemVer),
+        [Parameter(Mandatory = $false)][string]$Version = (Get-SemVer -IncludePrefix),
         [Parameter(Mandatory = $false)][string]$Message = "",
         [Parameter(Mandatory = $false)][string]$Prefix = "",
         [Parameter(Mandatory = $false)][switch]$Force = $false
     )
 
-    ($major, $minor, $patch, $pre, $prepatch, $build) = ConvertTo-SemVer -SemVersion $SemVersion | ForEach-Object { ($_.Major, $_.Minor, $_.Patch, $_.Pre, $_.PrePatch, $_.Build) }
+    ($major, $minor, $patch, $pre, $prepatch, $build, $verPrefix) = ConvertTo-SemVer -Version $Version |
+        ForEach-Object { ($_.Major, $_.Minor, $_.Patch, $_.Pre, $_.PrePatch, $_.Build, $_.Prefix) }
     switch ($SemVerb) {
         "major" {
             if (Test-String $pre) {
@@ -130,6 +138,11 @@ function Set-SemVer {
         }
     }
 
+    $prefixString = $Prefix
+    if (-Not(Test-String $prefixString)) {
+        $prefixString = $verPrefix
+    }
+
     # new semver
     $nextsemver = @{
         Major    = $major
@@ -138,17 +151,17 @@ function Set-SemVer {
         Pre      = $pre
         PrePatch = $prepatch
         Build    = $build
-    } | Format-SemVerString
+        Prefix   = $prefixString
+    } | Format-SemVerString -IncludePrefix
 
-
-    $setcmdPattern = "git tag '{1}v{0}'{2}"
+    $setcmdPattern = "git tag '{0}'{1}"
     $msgString = ""
     if (Test-String $Message) {
         $msgString = " -m '${Message}'"
     }
-    $setcmd = [scriptblock]::Create($setcmdPattern -f ($nextsemver, $Prefix, $msgString))
+    $setcmd = [scriptblock]::Create($setcmdPattern -f ($nextsemver, $msgString))
 
-    if ($PSCmdlet.ShouldProcess($SemVersion, "Set-SemVer")) {
+    if ($PSCmdlet.ShouldProcess($Version, "Set-SemVer")) {
         # Ensure no outstanding git commits
         if ($Force -or (Test-GitState -ShowMessage)) {
             Invoke-Command -ScriptBlock $setcmd
