d039ab1c420d6f7591887cf60c860b46f3f07379
Wael Rabadi <wrabadi@pivotal.io>
2019-10-12 02:11:21 -0500

    ensure src

--- jit-semver/install.ps1
+++ jit-semver/install.ps1
@@ -1,6 +1,7 @@
 param([switch]$WhatIf = $false, [switch]$Force = $false, [switch]$Verbose = $false)
 
-$psd1 = Get-ChildItem -Path $PSScriptRoot -Filter *.psd1 -File -Recurse | Select-Object -First 1
+$psd1 = Get-ChildItem -Path (join-path $PSScriptRoot -ChildPath src) `
+    -Filter *.psd1 -File -Recurse | Select-Object -First 1
 
 # Expect .\src\jit-semver.psd1
 Import-Module $psd1 -Force
