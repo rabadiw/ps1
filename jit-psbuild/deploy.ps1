c254f7cf90f48f2dc7dc09a6e3fd5967a4dbd64e
Wael Rabadi <wrabadi@pivotal.io>
2019-10-12 02:09:30 -0500

    correctly extract distpath

--- jit-psbuild/deploy.ps1
+++ jit-psbuild/deploy.ps1
@@ -3,7 +3,9 @@ function Deploy-JitPSBuild {
 
     $NuGetApiKeyPath = "..\.psgkey"
 
-    ($srcPath, $distPath) = Build-PSModule
+    # hashtable to array, not needed but good to know
+    # ($distPath, $srcPath) = [string[]]((Build-PSModule).Values | Out-String -Stream)
+    $distPath = (Build-PSModule).DistPath
     Set-PSModuleVersion -Path $distPath
 
     Publish-Module -Path $distPath -NuGetApiKey (get-content $NuGetApiKeyPath) -Verbose
