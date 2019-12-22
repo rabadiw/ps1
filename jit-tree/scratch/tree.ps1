962ff9939ae8d6586ba33e72f683231913025f7b
Wael Rabadi <wrabadi@pivotal.io>
2019-12-22 14:07:15 -0600

    grouped sratch files

--- jit-tree/scratch/tree.ps1
+++ jit-tree/scratch/tree.ps1
@@ -104,4 +104,4 @@ function Write-Tree {
     Write-Tree-Recursive -Path $Path -PathDepth 0 -Depth $Depth -Exclude $Exclude
 }
 
-Write-Tree "${PSScriptRoot}\.." -Exclude "objects"
+# Write-Tree "${PSScriptRoot}\.." -Exclude "objects"
