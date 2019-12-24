64a36e58d70e16b3da46129e034d3a7b92330114
Wael Rabadi <wael@mwrabadi.com>
2019-12-24 00:19:32 -0600

    jit-tree/  +attributes

--- jit-tree/src/jit-tree.psm1
+++ jit-tree/src/jit-tree.psm1
@@ -1,6 +1,7 @@
 . $PSScriptRoot\MessageFunctions.ps1
 
-function Write-Tree {
+function Write-Tree
+{
     [CmdletBinding()]
     param (
         # Specifies a path to one or more locations.
@@ -27,6 +28,11 @@ function Write-Tree {
             HelpMessage = "Specifies, as a string array, a property or property that this cmdlet excludes from the operation. The value of this parameter qualifies the Path parameter. Enter a path element or pattern, such as *.txt or A*. Wildcard characters are accepted.")]
         [string[]]$Exclude,
 
+        [Parameter(Mandatory = $false,
+            ParameterSetName = "Default",
+            HelpMessage = "Gets files and folders with the specified attributes. This parameter supports all attributes and lets you specify complex combinations of attributes.")]
+        [System.Management.Automation.FlagsExpression[System.IO.FileAttributes]]$Attributes,
+
         [Parameter(Mandatory = $false, ParameterSetName = "Default")]
         [switch]$DisplayHint
     )
@@ -42,9 +48,11 @@ function Write-Tree {
     # [3|0|Last]          │        └──jit-semver/
     # [0||Last]           └──templates/
 
-    function Write-Line($dir, $level, $bars, $isLast, $count) {
+    function Write-Line($dir, $level, $bars, $isLast, $count)
+    {
         $prefix = ""
-        for ($i = 0; $i -lt $level; $i++) {
+        for ($i = 0; $i -lt $level; $i++)
+        {
             if ($bars -contains $i) { $prefix += "│  " }
             else { $prefix += "   " }
         }
@@ -53,23 +61,27 @@ function Write-Tree {
         else { $prefix += "├──" }
         $itemMsg = "$prefix$($dir.Name)/"
 
-        if ($DebugPreference.value__) {
+        if ($DebugPreference.value__)
+        {
             $lastMsg = ("Not-Last", "Last")
             $debugMsg = "[$level|$($bars -join ', ')|$($lastMsg[$isLast])]  ".PadRight(20, ' ')
         }
-        if ($DisplayHint) {
+        if ($DisplayHint)
+        {
             $hintMsg = "[$($dir.Mode), $($dir.LastWriteTime.ToString("yyyy-MM-dd hh:mm tt")), $($count.ToString().PadLeft(6, ' '))]  "
         }
 
         Write-Output "${debugMsg}${hintMsg}${itemMsg}"
     }
 
-    function GetChildItem($path) {
-        return Get-ChildItem $path -Directory -Exclude $Exclude | Sort-Object -Property Name -Descending
+    function GetChildItem($path)
+    {
+        return Get-ChildItem $path -Directory -Exclude $Exclude -Attributes $Attributes | Sort-Object -Property Name -Descending
     }
 
     # Guard path
-    if (-Not(Test-Path $Path)) {
+    if (-Not(Test-Path $Path))
+    {
         Write-Information -MessageData (Format-ErrorMessage "Not a valid path $Path") -InformationAction Continue
         break
     }
@@ -77,7 +89,8 @@ function Write-Tree {
     $currentDir = Resolve-Path -Path $Path
     Write-Output $currentDir.Path
 
-    if ($DisplayHint.IsPresent) {
+    if ($DisplayHint.IsPresent)
+    {
         Write-Output "Columns: [Mode, LastWriteTime, Count, Name]"
     }
 
@@ -89,7 +102,8 @@ function Write-Tree {
     $subDirs | Select-Object -First 1 | ForEach-Object { $dir.Push(@( $_, $true, $subDir.Count, [int]$null, [int[]]$null)) }
     $subDirs | Select-Object -Skip 1 | ForEach-Object { $dir.Push(@( $_, $false, $subDir.Count, [int]$null, [int[]]$null)) }
 
-    while ($dir.Count -gt 0) {
+    while ($dir.Count -gt 0)
+    {
         ($currentDir, $isLast, $count, $level, $bars) = $dir.Pop()
 
         if (($Depth -ne -1) -and ($level -gt $Depth - 1)) { continue }
@@ -98,11 +112,14 @@ function Write-Tree {
 
         $subBar = [int[]]$bars
         $hasChildren = (GetChildItem -path $currentDir.FullName).Count -gt 0
-        if (-not($isLast) -and $hasChildren) {
-            if ($null -eq $subBar) {
+        if (-not($isLast) -and $hasChildren)
+        {
+            if ($null -eq $subBar)
+            {
                 $subBar = [int[]]($level)
             }
-            else {
+            else
+            {
                 $subBar = $subBar + $level
             }
         }
