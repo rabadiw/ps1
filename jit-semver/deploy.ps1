2517f8971c187eb668a644338c4852d5bec2ce3b
Wael Rabadi <wael@mwrabadi.com>
2020-03-03 22:32:01 -0600

    jit-semver/ updated deploy script

--- jit-semver/deploy.ps1
+++ jit-semver/deploy.ps1
@@ -17,11 +17,12 @@ function Deploy-JitSemVer {
 
 function Set-JitSemVerTag {
 
-    param([switch]$WhatIf = $false, [switch]$Force = $false, [switch]$Verbose = $false)
+    param([switch]$WhatIf = $false, [switch]$Force = $false, [switch]$Verbose = $false,
+    [ValidateSet("major", "minor", "patch", "build")][string]$SemVerb = "patch")
 
     $semverprefix = "jit-semver"
     $logPath = join-path $PSScriptRoot -ChildPath CHANGELOG.md
-    $semver = Get-SemVerNext -Version (Get-SemVer -Filter $semverprefix) -Prefix "$semverprefix/v"
+    $semver = Get-SemVerNext -Version (Get-SemVer -Filter $semverprefix) -Prefix "$semverprefix/v" -SemVerb $SemVerb
     $msg = Get-SemVerChangeSummary -ChangeLogPath $logPath -Version $semver | Select-Object -ExpandProperty Content
 
     Set-SemVer `
